534517e01690af610e718dbe97a09b1e992a5140
BEELZ <othmanmuhidean@gmail.com>
2020-09-01 14:19:45 -0700

    Results display when pressing enter key!

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -22,88 +22,107 @@ function Books() {
     .catch(err => console.log(err));
   }
 
-  function deleteBook(){
-    API.deleteBook()
-    .then(res=>loadBooks())
-    .catch(err=>console.log(err))
-  }
 
   function handleChange(event){
     const {value} = event.target
     
     setQuery(value)
-console.log("Your query is:",query)
+// console.log("Your query is:",query)
 
   }
- 
+  function handleSubmit(event){
+    event.preventDefault()
+    if(query){
+      let url = `https://www.googleapis.com/books/v1/volumes?q=${query}`
+      axios.get(url)
+   .then(res => {
+     
+      setBooks(res.data.items)
+      
+    })
+    .catch(err=>{console.log(err)})
+    }  
+    
+  }
+//  console.log(books)
+
+function handleEnterKey(event){
+  if(event.target.value === "Enter"){
+    
+    handleSubmit()
+  }
+
+}
+
   return ( 
-//   <Container fluid>
-//     <Row>
+  <Container fluid>
+    <Row>
    
-//     <Col size="md-12">
-//        <Jumbotron>
-//       <h1>USE GOOGLE BOOKS</h1>
-//     </Jumbotron>
-//     <form>
+    <Col size="md-12">
+       <Jumbotron>
+      <h1>USE GOOGLE BOOKS</h1>
+    </Jumbotron>
+    <form>
      <Fragment> 
       <Input
         autoComplete="off" 
         type="text"
         value={query}
         onChange={handleChange}
+        onKeyPress={handleEnterKey}
         
-      /> </Fragment>
-//       <FormBtn
-//         type="button"
-//         // onClick={handleSubmit}
-//       >
-//         Search
-//       </FormBtn>
-      
-//  </form>
-//       <ul>
-//         {data.items.map(item => (
-//         <li key={item.id}>
-//           <Row>
-//             <Col size="sm-2">
+      /> 
+       <FormBtn
+         type="submit"
+         onClick={handleSubmit}
+       >
+         Search
+      </FormBtn>
+      </Fragment>
+  </form>
+       <ul>
+         {books.map(book => (
+        <li key={book.id}>
+          <Row>
+            <Col size="sm-2">
               
-//               <img src={item.volumeInfo && 
-//               item.volumeInfo.imageLinks && 
-//               item.volumeInfo.imageLinks.thumbnail ?
-//               item.volumeInfo.imageLinks.thumbnail : "#"}
-//               alt={item.volumeInfo.title ? 
-//                 item.volumeInfo.title : ["No known title"]}>
-//               </img>
+              <img src={book.volumeInfo && 
+              book.volumeInfo.imageLinks && 
+              book.volumeInfo.imageLinks.thumbnail ?
+              book.volumeInfo.imageLinks.thumbnail : "#"}
+              alt={book.volumeInfo.title ? 
+                book.volumeInfo.title : ["No known title"]}>
+              </img>
 
-//             </Col>
-//             <Col size="sm-10">
+            </Col>
+            <Col size="sm-10">
 
-//               <h3>{item.volumeInfo.title ? 
-//               item.volumeInfo.title : ["No known title"]}</h3>
+              <h3>{book.volumeInfo.title ? 
+              book.volumeInfo.title : ["No known title"]}</h3>
 
-//               <strong>{item.volumeInfo.authors ? item.volumeInfo.authors.join(",") : ["No known Authors"]}</strong>
+              <strong>{book.volumeInfo.authors ? book.volumeInfo.authors.join(",") : ["No known Authors"]}</strong>
         
-//               <p>{item.volumeInfo.description ? item.volumeInfo.description : ["No description available"] }</p>
+              <p>{book.volumeInfo.description ? book.volumeInfo.description : ["No description available"] }</p>
               
-//               <a href={item.volumeInfo.infoLink}>More Info</a>
+              <a href={book.volumeInfo.infoLink}>More Info</a>
               
-//               <SaveButton
-//               title={item.volumeInfo.title ? item.volumeInfo.title : ["No known title"]}
-//               authors={item.volumeInfo.authors ? item.volumeInfo.authors.join(",") : ["No known Authors"]}
-//               />
-//             </Col>
-//          </Row>
-//         </li>
+              <SaveButton
+              title={book.volumeInfo.title ? book.volumeInfo.title : ["No known title"]}
+              authors={book.volumeInfo.authors ? book.volumeInfo.authors.join(",") : ["No known Authors"]}
+              />
+            </Col>
+         </Row>
+        </li>
           
-//         ))}
-//       </ul>
+        ))}
+      </ul>
     
     
-//     </Col>
-//     </Row>
+    </Col>
+    </Row>
 
 
-//   </Container>
+  </Container>
    
   );
   }
