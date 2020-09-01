8ed305033202d74f049549811144497d9440b1b0
BEELZ <othmanmuhidean@gmail.com>
2020-09-01 13:29:48 -0700

    Redoing search bar page, to optimize efficiency

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,118 +1,110 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import axios from 'axios';
 import Jumbotron from "../components/Jumbotron";
 import SaveButton from "../components/SaveButton"
 import { Col, Row, Container } from "../components/Grid";
 import { Input, FormBtn } from "../components/Form";
-// import API from "../utils/API";
+import API from "../utils/API";
 
 function Books() {
-  const [data, setData] = useState({ items: [] });
-  const [query, setQuery] = useState(' ');
-  const [url, setUrl] = useState(
-    'https://www.googleapis.com/books/v1/volumes?q=',
-  );
- 
+  const [books, setBooks] = useState([])
+  const [query, setQuery] = useState("")
 
-  function handleSubmit(event){
-    event.preventDefault()
-    setUrl(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+  useEffect(()=>{
+    loadBooks()
+  },[])
+
+  function loadBooks(){
+    API.getBooks()
+    .then(res => 
+    setBooks(res.data)
+    )
+    .catch(err => console.log(err));
+  }
+
+  function deleteBook(){
+    API.deleteBook()
+    .then(res=>loadBooks())
+    .catch(err=>console.log(err))
   }
 
   function handleChange(event){
-    event.preventDefault()
-    setQuery(event.target.value)
+    const {value} = event.target
     
+    setQuery(value)
+console.log("Your query is:",query)
+
   }
  
-   function submitOnEnter(event) {
-    if(event.key === "Enter"){
-       handleSubmit()
-     }
-   }
-
-  
- 
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(url);
-      console.log(result.data)
-      setData(result.data);
-    };
- 
-    fetchData();
-  }, [url]);
- 
   return ( 
-  <Container fluid>
-    <Row>
+//   <Container fluid>
+//     <Row>
    
-    <Col size="md-12">
-       <Jumbotron>
-      <h1>USE GOOGLE BOOKS</h1>
-    </Jumbotron>
-    <form>
-      
+//     <Col size="md-12">
+//        <Jumbotron>
+//       <h1>USE GOOGLE BOOKS</h1>
+//     </Jumbotron>
+//     <form>
+     <Fragment> 
       <Input
         autoComplete="off" 
         type="text"
         value={query}
         onChange={handleChange}
-        onKeyPress={submitOnEnter}
-      />
-      <FormBtn
-        type="button"
-        onClick={handleSubmit}
-      >
-        Search
-      </FormBtn>
-      
- </form>
-      <ul>
-        {data.items.map(item => (
-        <li key={item.id}>
-          <Row>
-            <Col size="sm-2">
-              
-              <img src={item.volumeInfo && 
-              item.volumeInfo.imageLinks && 
-              item.volumeInfo.imageLinks.thumbnail ?
-              item.volumeInfo.imageLinks.thumbnail : "#"}
-              alt={item.volumeInfo.title ? 
-                item.volumeInfo.title : ["No known title"]}>
-              </img>
-
-            </Col>
-            <Col size="sm-10">
-
-              <h3>{item.volumeInfo.title ? 
-              item.volumeInfo.title : ["No known title"]}</h3>
-
-              <strong>{item.volumeInfo.authors ? item.volumeInfo.authors.join(",") : ["No known Authors"]}</strong>
         
-              <p>{item.volumeInfo.description ? item.volumeInfo.description : ["No description available"] }</p>
+      /> </Fragment>
+//       <FormBtn
+//         type="button"
+//         // onClick={handleSubmit}
+//       >
+//         Search
+//       </FormBtn>
+      
+//  </form>
+//       <ul>
+//         {data.items.map(item => (
+//         <li key={item.id}>
+//           <Row>
+//             <Col size="sm-2">
               
-              <a href={item.volumeInfo.infoLink}>More Info</a>
+//               <img src={item.volumeInfo && 
+//               item.volumeInfo.imageLinks && 
+//               item.volumeInfo.imageLinks.thumbnail ?
+//               item.volumeInfo.imageLinks.thumbnail : "#"}
+//               alt={item.volumeInfo.title ? 
+//                 item.volumeInfo.title : ["No known title"]}>
+//               </img>
+
+//             </Col>
+//             <Col size="sm-10">
+
+//               <h3>{item.volumeInfo.title ? 
+//               item.volumeInfo.title : ["No known title"]}</h3>
+
+//               <strong>{item.volumeInfo.authors ? item.volumeInfo.authors.join(",") : ["No known Authors"]}</strong>
+        
+//               <p>{item.volumeInfo.description ? item.volumeInfo.description : ["No description available"] }</p>
               
-              <SaveButton
-              title={item.volumeInfo.title ? item.volumeInfo.title : ["No known title"]}
-              authors={item.volumeInfo.authors ? item.volumeInfo.authors.join(",") : ["No known Authors"]}
-              />
-            </Col>
-         </Row>
-        </li>
+//               <a href={item.volumeInfo.infoLink}>More Info</a>
+              
+//               <SaveButton
+//               title={item.volumeInfo.title ? item.volumeInfo.title : ["No known title"]}
+//               authors={item.volumeInfo.authors ? item.volumeInfo.authors.join(",") : ["No known Authors"]}
+//               />
+//             </Col>
+//          </Row>
+//         </li>
           
-        ))}
-      </ul>
+//         ))}
+//       </ul>
     
     
-    </Col>
-    </Row>
+//     </Col>
+//     </Row>
 
 
-  </Container>
-    
+//   </Container>
+   
   );
   }
 
